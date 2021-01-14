b1912786dc84b6d5559a96e3a7d4b050990ac972
John Bellone <jbellone@sscinc.com>
2021-01-14 15:06:00 -0500

    Adds tags and sapns.

--- server_interceptors.go
+++ server_interceptors.go
@@ -4,7 +4,9 @@ import(
 	"context"
 
 	"github.com/getsentry/sentry-go"
+
 	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_tags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
 
 	"google.golang.org/grpc"
 )
@@ -35,13 +37,20 @@ func UnaryServerInterceptor(opts ...ServerOption) grpc.UnaryServerInterceptor {
 			ctx = sentry.SetHubOnContext(ctx, hub)
 		}
 
-		// TODO: Add tags for metadata information.
+		span := sentry.StartSpan(ctx, "grpc.server")
+		defer span.Finish()
+
 		// TODO: Perhaps makes sense to use SetRequestBody instead?
-		hub.Scope().SetExtra("requestBody", req)
+		hub.Scope().SetTransaction(info.FullMethod)
 		defer recoverWithSentry(hub, ctx, o)
 
 		resp, err := handler(ctx, req)
 		if o.ReportOn(err) {
+			tags := grpc_tags.Extract(ctx)
+			for k, v := range tags.Values() {
+				hub.Scope().SetTag(k, v.(string))
+			}
+
 			hub.CaptureException(err)
 		}
 
@@ -66,10 +75,16 @@ func StreamServerInterceptor(opts ...ServerOption) grpc.StreamServerInterceptor 
 		stream := grpc_middleware.WrapServerStream(ss)
 		stream.WrappedContext = ctx
 
+		hub.Scope().SetTransaction(info.FullMethod)
 		defer recoverWithSentry(hub, ctx, o)
 
 		err := handler(srv, stream)
-		if err != nil && o.ReportOn(err) {
+		if o.ReportOn(err) {
+			tags := grpc_tags.Extract(ctx)
+			for k, v := range tags.Values() {
+				hub.Scope().SetTag(k, v.(string))
+			}
+
 			hub.CaptureException(err)
 		}
 
